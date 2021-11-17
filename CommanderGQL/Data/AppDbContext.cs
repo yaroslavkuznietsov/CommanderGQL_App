9c15f9e27819ab29d055234e0d817f0db3753e59
Yaroslav <yaroslav.kuznietsov@gmail.com>
2021-11-18 01:39:03 +0200

    First push of app with added dependency injection, sql connection & DbContext

---
+++ CommanderGQL/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+using CommanderGQL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommanderGQL.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+
+        public DbSet<Platform> Platforms { get; set; }
+    }
+}
