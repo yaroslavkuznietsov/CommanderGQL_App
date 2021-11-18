816b2d5b5c55220bddad69af9bf6a254a5107968
Yaroslav <yaroslav.kuznietsov@gmail.com>
2021-11-18 23:44:21 +0200

    Added Query class for DbContext

---
+++ CommanderGQL/GraphQL/Query.cs
@@ -0,0 +1,15 @@
+﻿using CommanderGQL.Data;
+using CommanderGQL.Models;
+using HotChocolate;
+using System.Linq;
+
+namespace CommanderGQL.GraphQL
+{
+    public class Query
+    {
+        public IQueryable<Platform> GetPlatform([Service] AppDbContext context)
+        {
+            return context.Platforms;
+        }
+    }
+}
