22cdd30ee40ba6a5e53a8ce25cacc081a3e079dc
Yaroslav <yaroslav.kuznietsov@gmail.com>
2021-11-21 01:35:06 +0200

    Added GraphQLDescription attributes to Platform class properties

--- CommanderGQL/Models/Platform.cs
+++ CommanderGQL/Models/Platform.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using HotChocolate;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 
 namespace CommanderGQL.Models
 {
+    [GraphQLDescription("Repesents any software or service that has a command line interface.")]
     public class Platform
     {
         [Key]
@@ -11,7 +13,8 @@ namespace CommanderGQL.Models
 
         [Required]
         public string Name { get; set; }
-        
+
+        [GraphQLDescription("Reprents a purchased, valid license for the platform.")]
         public string LicenseKey { get; set; }
 
         public ICollection<Command> Commands { get; set; } = new List<Command>();
