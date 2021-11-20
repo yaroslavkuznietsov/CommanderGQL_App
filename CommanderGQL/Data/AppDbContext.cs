7497d90d45c95c5e5989930c0b9af560a2977903
Yaroslav <yaroslav.kuznietsov@gmail.com>
2021-11-20 21:52:12 +0200

    Added DbSet<Command> + override OnModelCreating for proper migration

--- CommanderGQL/Data/AppDbContext.cs
+++ CommanderGQL/Data/AppDbContext.cs
@@ -12,5 +12,19 @@ namespace CommanderGQL.Data
         }
 
         public DbSet<Platform> Platforms { get; set; }
+        public DbSet<Command> Commands { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Platform>()
+                        .HasMany(p => p.Commands)
+                        .WithOne(p => p.Platform!)
+                        .HasForeignKey(p => p.PlatformId);
+
+            modelBuilder.Entity<Command>()
+                        .HasOne(p => p.Platform)
+                        .WithMany(p => p.Commands)
+                        .HasForeignKey(p => p.PlatformId);
+        }
     }
 }
