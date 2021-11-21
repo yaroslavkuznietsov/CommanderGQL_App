d043fec4f2b9ef1fb92ae3cf3261cf7129e34b7f
Yaroslav <yaroslav.kuznietsov@gmail.com>
2021-11-21 19:23:54 +0200

    Added ./GraphQL/Platfoms/ to add HotChocolate Types for segregation

---
+++ CommanderGQL/GraphQL/Platforms/PlatformType.cs
@@ -0,0 +1,32 @@
+﻿using CommanderGQL.Data;
+using CommanderGQL.Models;
+using HotChocolate;
+using HotChocolate.Types;
+using System.Linq;
+
+namespace CommanderGQL.GraphQL.Platforms
+{
+    public class PlatformType : ObjectType<Platform>
+    {
+        protected override void Configure(IObjectTypeDescriptor<Platform> descriptor)
+        {
+            descriptor.Description("Repesents any software or service that has a command line interface.");
+
+            descriptor.Field(p => p.LicenseKey).Ignore();
+
+            descriptor.Field(p => p.Commands)
+                      .ResolveWith<Resolvers>(p => p.GetCommands(default!, default!))
+                      .UseDbContext<AppDbContext>()
+                      .Description("This is list of available commands for this platform");
+        }
+
+
+        private class Resolvers
+        {
+            public IQueryable<Command> GetCommands(Platform platform, [ScopedService] AppDbContext context)
+            {
+                return context.Commands.Where(c => c.PlatformId == platform.Id);
+            }
+        }
+    }
+}
