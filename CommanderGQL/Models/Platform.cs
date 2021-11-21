e094a8485cbcbb2f419f60e20d763e7fa0b317dd
Yaroslav <yaroslav.kuznietsov@gmail.com>
2021-11-21 19:25:34 +0200

    Removed fields description and moved it to PlatformType class

--- CommanderGQL/Models/Platform.cs
+++ CommanderGQL/Models/Platform.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace CommanderGQL.Models
 {
-    [GraphQLDescription("Repesents any software or service that has a command line interface.")]
+    //[GraphQLDescription("Repesents any software or service that has a command line interface.")] / использовалось для описания api voyager /
     public class Platform
     {
         [Key]
@@ -14,7 +14,7 @@ namespace CommanderGQL.Models
         [Required]
         public string Name { get; set; }
 
-        [GraphQLDescription("Reprents a purchased, valid license for the platform.")]
+        //[GraphQLDescription("Reprents a purchased, valid license for the platform.")]
         public string LicenseKey { get; set; }
 
         public ICollection<Command> Commands { get; set; } = new List<Command>();
