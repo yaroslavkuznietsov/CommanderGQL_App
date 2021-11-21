73eb34dbf2032718fa6e3718213b013b9959996d
Yaroslav <yaroslav.kuznietsov@gmail.com>
2021-11-22 01:55:12 +0200

    Added CommandType class

---
+++ CommanderGQL/GraphQL/Commands/CommandType.cs
@@ -0,0 +1,29 @@
+﻿using CommanderGQL.Data;
+using CommanderGQL.Models;
+using HotChocolate;
+using HotChocolate.Types;
+using System.Linq;
+
+namespace CommanderGQL.GraphQL.Commands
+{
+    public class CommandType : ObjectType<Command>
+    {
+        protected override void Configure(IObjectTypeDescriptor<Command> descriptor)
+        {
+            descriptor.Description("Reperesents any executable command");
+
+            descriptor.Field(c => c.Platform)
+                      .ResolveWith<Resolvers>(c => c.GetPlatform(default!, default!))
+                      .UseDbContext<AppDbContext>()
+                      .Description("This is the platform to which command belongs");
+        }
+
+        private class Resolvers
+        {
+            public Platform GetPlatform([Parent] Command command, [ScopedService] AppDbContext context)
+            {
+                return context.Platforms.FirstOrDefault(p => p.Id == command.PlatformId);
+            }
+        }
+    }
+}
